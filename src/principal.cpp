ac7d224f633804abda2e7a311e69520439af1870
JuanjoMrt <juanjomartin97@hotmail.es>
2016-11-08 18:59:37 +0100

    Bug fixes in principal.cpp

--- src/principal.cpp
+++ src/principal.cpp
@@ -13,7 +13,7 @@ using namespace std;
   @param[in,out] cm objeto tipo conjunto sobre el que se almacenan las mutaciones
 	@return true si la lectura ha sido correcta, false en caso contrario
 	*/
-/*
+
 bool load(conjunto &  cm, const string & s) {
  ifstream fe;
  string cadena;
@@ -35,7 +35,7 @@ bool load(conjunto &  cm, const string & s) {
         // Invoco el constructor de mutación que recibe una cadena completa, la parsea y crea la mutación.
         mutacion mut = mutacion(cadena);
         // Insertar mutación en el conjunto
-        cm.insert(mut);
+        cm.insert(mut);						//Cambiado insert por pushback para mejorar la eficiencia
         getline(fe,cadena,'\n');
     }
     fe.close();
@@ -45,14 +45,15 @@ bool load(conjunto &  cm, const string & s) {
   return false;
  }
  void AEncontrarConString(string a_encontrar){
-    bool encontrado;
+ 	conjunto conjuntoMutaciones;
+ 	conjunto::value_type mutacion;
     pair < conjunto::value_type, bool > par_encontrar = conjuntoMutaciones.find(a_encontrar);
-    if(get<2>(par_encontrar)){
-		mutacion = get<1>(par_encontrar);
+    if(par_encontrar.second){
+		mutacion = par_encontrar.first;
     	cout << "\n" << mutacion << " y sus enfermedades asociadas son: ";
-    	vector<enfermedad> enfermedades_mutacion = mutacion.getEnfermedad();
-    	for(int i = 0; i < enfermedades_mutacion.size(); i++){
-    		cout << enfermedades_mutacion.get(i) "\n";
+    	vector<enfermedad> enfermedades_mutacion = mutacion.getEnfermedades();
+    	for(unsigned int i = 0; i < enfermedades_mutacion.size(); i++){
+    		cout << enfermedades_mutacion.at(i) << "\n";
     	}
     }
     else{
@@ -60,18 +61,19 @@ bool load(conjunto &  cm, const string & s) {
     }
  }
 void AEncontrarConChar(string cromosoma, unsigned int posicion){
-	bool encontrado;
+	conjunto conjuntoMutaciones;
+	conjunto::value_type mutacion;
     pair < conjunto::value_type, bool > par_encontrar = conjuntoMutaciones.find(cromosoma, posicion);
-    if(get<2>(par_encontrar)){
-    	mutacion = get<1>(par_encontrar);
+    if(par_encontrar.second){
+    	mutacion = par_encontrar.first;
 		cout << "\n" << mutacion << " y sus enfermedades asociadas son: ";
-		vector<enfermedad> enfermedades_mutacion = mutacion.getEnfermedad();
-    	for(int i = 0; i < enfermedades_mutacion.size(); i++){
-    		cout << enfermedades_mutacion.get(i) "\n";
+		vector<enfermedad> enfermedades_mutacion = mutacion.getEnfermedades();
+    	for(unsigned int i = 0; i < enfermedades_mutacion.size(); i++){
+    		cout << enfermedades_mutacion.at(i) << "\n";
     	}
     }
     else{
-    	cout << "\nNo se ha encontrado la mutación.\n"
+    	cout << "\nNo se ha encontrado la mutación.\n";
     }
 
 }
@@ -79,7 +81,7 @@ void AEncontrarConChar(string cromosoma, unsigned int posicion){
 int CalculaMutaciones(string numero_cromosoma){
 	int nmutaciones = 0;									//Contador de  mutaciones de dicho cromosoma
 	conjunto conjuntoMutaciones;							
-	value_type mutacion = *conjuntoMutaciones.begin();
+	conjunto::value_type mutacion = *conjuntoMutaciones.begin();
 	bool nmmdec = false; 									//No hay más mutaciones de ese char
 	bool fin_conjunto = false; 								//nos indica si el conjunto ha terminado
 
@@ -91,41 +93,42 @@ int CalculaMutaciones(string numero_cromosoma){
 	fin_conjunto = conjuntoMutaciones.upper_bound(mutacion) < conjuntoMutaciones.end();
 	mutacion = *conjuntoMutaciones.upper_bound(mutacion);	//cargamos la siguiente mutacion
 	}
-}*/
+	return nmutaciones; 
+}
 
 
 int main(int argc, char *argv[]){
-//
-//    conjunto conjuntoMutaciones;
-//    string query_str; 
-//    value_type mutacion;
-//
+
+    conjunto conjuntoMutaciones;
+    string query_str; 
+    conjunto::value_type mutacion;
+
     //Cargar las mutaciones en vectorSNPs
-//    load(conjuntoMutaciones, "clinvar_20160831.vcf");
+    load(conjuntoMutaciones, "clinvar_20160831.vcf");
     
     //Imprimir número de elementos almacenados en conjuntoMutaciones
-//    cout << "Lectura del fichero finalizada. Mutaciones cargadas: " << conjuntoMutaciones.size() << endl;  
+    cout << "Lectura del fichero finalizada. Mutaciones cargadas: " << conjuntoMutaciones.size() << endl;  
 
     //Imprimir cuántas mutaciones están asociadas al cromosoma 1:
-//    string numero_cromosoma = "1";
-//    int numero_mutaciones_1 = CalculaMutaciones(numero_cromosoma);
-//    cout << "Mutaciones asociadas a Chr 1: " << numero_mutaciones_1 << endl; 
+    string numero_cromosoma = "1";
+    int numero_mutaciones_1 = CalculaMutaciones(numero_cromosoma);
+    cout << "Mutaciones asociadas a Chr 1: " << numero_mutaciones_1 << endl; 
 
     /**@todo ¿Existe la mutación con ID "rs147165522"? Imprimir la mutación y las enfermedades asociadas */
-//    string id_a_encontrar = "rs147165522";
-//    AEncontrarConString( string id_a_encontrar);
+    string id_a_encontrar = "rs147165522";
+    AEncontrarConString(id_a_encontrar);
     
     /**@todo ¿Existe la mutación en chr/pos "14"/67769578? Imprimir la mutación y las enfermedades asociadas */	
-//	string cromosoma_a_buscar = "14";
-//	unsigned int pos_a_buscar = 67769578;
-//	AEncontrarConChar(cromosoma_a_buscar, pos_a_buscar);
+	string cromosoma_a_buscar = "14";
+	unsigned int pos_a_buscar = 67769578;
+	AEncontrarConChar(cromosoma_a_buscar, pos_a_buscar);
     
     /**@todo ¿Cómo podríamos calcular el número de mutaciones del cromosoma 3? (utiliza lower_bound / upper_bound) */
-//	numero_cromosoma = "3";
-//	int numero_mutaciones = CalculaMutaciones(numero_cromosoma);
-//	cout << "\nEl número de mutaciones del cromosoma 3 es: " << numero_mutaciones << "\n";
+   	numero_cromosoma = "3";
+	int numero_mutaciones = CalculaMutaciones(numero_cromosoma);
+	cout << "\nEl número de mutaciones del cromosoma 3 es: " << numero_mutaciones << "\n";
 
     /**@todo Analiza la eficiencia teórica y empírica de las operaciones find, insert y erase */
 
-//   return 0;
+   return 0;
 }
